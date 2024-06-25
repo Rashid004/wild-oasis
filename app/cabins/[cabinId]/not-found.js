7fbc9308fce10183cc91437ba52f7a7ca3af079c
Your Name <you@example.com>
2024-06-25 13:12:21 +0530

    sharing state Client server

--- app/cabins/[cabinId]/not-found.js
+++ app/cabins/[cabinId]/not-found.js
@@ -1,17 +1,19 @@
 /** @format */
 
+import Link from "next/link";
+
 function NotFound() {
   return (
     <main className="text-center space-y-6 mt-4">
       <h1 className="text-3xl font-semibold">
         This page could not be found :(
       </h1>
-      <a
+      <Link
         href="/cabins"
         className="inline-block bg-accent-500 text-primary-800 px-6 py-3 text-lg"
       >
         back to all cabins
-      </a>
+      </Link>
     </main>
   );
 }
