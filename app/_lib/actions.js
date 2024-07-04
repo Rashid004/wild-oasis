b3be63fbbc0447ab15adb10cbcfa2e6e7d5d3a39
Your Name <you@example.com>
2024-07-04 13:54:06 +0530

    Final Commit

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,36 +1,58 @@
 /** @format */
 
 "use server";
-import { revalidatePath } from "next/cache";
+
 import { auth, signIn, signOut } from "./auth";
-import { supabase } from "./supabase";
 import { getBookings } from "./data-service";
+import { supabase } from "./supabase";
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 export async function updateGuest(formData) {
   const session = await auth();
-
-  if (!session) throw new Error("You must be loged in");
+  if (!session) throw new Error("You must be logged in");
 
   const nationalID = formData.get("nationalID");
-
   const [nationality, countryFlag] = formData.get("nationality").split("%");
 
-  const regex = /^[a-zA-Z0-9]{6,12}$/;
-  if (!regex.test(nationalID)) throw new Error("Please provide a national ID");
+  if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID))
+    throw new Error("Please provide a valid national ID");
 
-  const updateData = { nationalID, nationality, countryFlag };
+  const updateData = { nationality, countryFlag, nationalID };
 
   const { data, error } = await supabase
     .from("guests")
     .update(updateData)
-    .eq("id", session.user.guestId)
-    .select()
-    .single();
+    .eq("id", session.user.guestId);
 
   if (error) throw new Error("Guest could not be updated");
 
   revalidatePath("/account/profile");
+}
+
+export async function createBooking(bookingData, formData) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const newBooking = {
+    ...bookingData,
+    guestId: session.user.guestId,
+    numGuests: Number(formData.get("numGuests")),
+    observations: formData.get("observations").slice(0, 1000),
+    extrasPrice: 0,
+    totalPrice: bookingData.cabinPrice,
+    isPaid: false,
+    hasBreakfast: false,
+    status: "unconfirmed",
+  };
+
+  const { error } = await supabase.from("bookings").insert([newBooking]);
+
+  if (error) throw new Error("Booking could not be created");
+
+  revalidatePath(`/cabins/${bookingData.cabinId}`);
+
+  redirect("/cabins/thankyou");
 }
 
 export async function deleteBooking(bookingId) {
